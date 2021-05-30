5e40384c1f380afa305a3776970f0331b5b3861b
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-05-30 15:41:13 +0300

    added a test

--- user.py
+++ user.py
@@ -10,4 +10,11 @@ class User:
     def __init__(self,user_name,password):
         self.user_name = user_name
         self.password = password
+
+    def save_user(self):
+
+        User.user_list.append(self)
+    
+    def tearDown(self):
+        User.user_list = []
         
