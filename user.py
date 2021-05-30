797a595989834bebadc3b0d41833997e5ca17155
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-05-30 11:09:10 +0300

    add a user.py file

---
+++ user.py
@@ -0,0 +1,18 @@
+# import pyperclip #importing pyperclip for copying to clipboard
+
+class User:
+    '''
+    class that allows user to generate user account
+    '''
+    
+    user_list = []
+    
+    def __init__(self,user_name,password):
+        self.user_name = user_name
+        self.password = password
+        
+    # def save_user(self):
+    #     '''
+    #     A method that saves new user object
+    #     '''
+    #     User.user_list.append(self)
