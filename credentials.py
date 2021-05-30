c4758cc15c2f63b1d511179c6d6fd4e196babd13
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-05-30 22:25:48 +0300

    modified a pychahe

--- credentials.py
+++ credentials.py
@@ -6,8 +6,21 @@ class Credentials:
     '''
     credential_list = []
     def __init__(self,user_name,account_name,password):
-        '''method to define the properties
+        '''
+        method to define the properties
         '''
         self.user_name = user_name
         self.account_name = account_name
-        self.password
+        self.password = password
+
+    def save_credentials(self):
+        '''
+        A method that saves new user object
+        '''
+        Credentials.credential_list.append(self)
+    
+    def delete_credentials(self):
+        Credentials.credential_list.remove(self)
+    
+    
+
